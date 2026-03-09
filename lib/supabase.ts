96ac00a35ab1b5a3fdfd1017aa882dad770b4925
Naphat <naphatsakorn.bu@kkumail.com>
2026-03-10 02:52:50 +0700

    fix supabase env

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,9 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+const supabaseUrl =
+  process.env.NEXT_PUBLIC_SUPABASE_URL || "https://zzwtajhzdjtwqeszswrp.supabase.co";
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+const supabaseAnonKey =
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp6d3Rhamh6ZGp0d3Flc3pzd3JwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI3NzkwMDksImV4cCI6MjA4ODM1NTAwOX0.4tLWGSRhugqb4pIvfBgzsr67dT__q-QAJbFTYxHRDcE";
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
